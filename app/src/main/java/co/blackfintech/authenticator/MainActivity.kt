066822a5256fb171757155efde8b344bb3c12b1b
DESKTOP-6BDRUUQ\DEV PARTNERS <alcyjoy10@gmail.com>
2017-10-03 12:28:01 +0800

    completed main activity with logout functionality

--- app/src/main/java/co/blackfintech/authenticator/MainActivity.kt
+++ app/src/main/java/co/blackfintech/authenticator/MainActivity.kt
@@ -2,6 +2,14 @@ package co.blackfintech.authenticator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import co.blackfintech.authenticator.util.SessionStore
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.yesButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +17,39 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
-}
+
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.main_menu, menu)
+
+    return true
+
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean
+  {
+    // listen for onclick event of the menu items
+    when(item?.itemId) {
+      R.id.action_logout -> {
+
+        logout()
+
+      }
+    }
+    return true
+  }
+  fun logout() {
+    alert ("Are you sure you want to logout?") {
+      yesButton {
+       val session = SessionStore()
+        session.clearEmail(this@MainActivity)
+
+        startActivity<LoginActivity>()
+
+        finish()
+      }
+    }.show()
+  }
+  }
+
+
